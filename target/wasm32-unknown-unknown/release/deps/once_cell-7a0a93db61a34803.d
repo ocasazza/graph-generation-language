e9947ef42d1f73e39e463d04cc9f77967d67da15
Olive Casazza <olive.casazza@schrodinger.com>
2025-07-01 22:29:55 -0700

    remove target

--- target/wasm32-unknown-unknown/release/deps/once_cell-7a0a93db61a34803.d
+++
@@ -1,7 +0,0 @@
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/libonce_cell-7a0a93db61a34803.rmeta: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs
-
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/libonce_cell-7a0a93db61a34803.rlib: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs
-
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/once_cell-7a0a93db61a34803.d: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs
-
-/Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/once_cell-1.21.3/src/lib.rs:
--- target/wasm32-unknown-unknown/release/deps/ucd_trie-37c3176f5ba58197.d
+++
@@ -1,8 +0,0 @@
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/libucd_trie-37c3176f5ba58197.rmeta: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/libucd_trie-37c3176f5ba58197.rlib: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/Users/casazza/Code/jump-cannon/wasm/ggl/target/wasm32-unknown-unknown/release/deps/ucd_trie-37c3176f5ba58197.d: /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs /Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs
-
-/Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/lib.rs:
-/Users/casazza/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ucd-trie-0.1.7/src/owned.rs:
